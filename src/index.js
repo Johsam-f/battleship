4a97fa3528e8c0ddd3e1744a6af197dc4bc9808a
Johsam <johsamfcress@gmail.com>
2025-06-02 21:47:53 +0200

    done login page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './style/main.css';
+import './style/fonts.css';
 
 console.log('Battleship is ready to sail!');
---
+++ src/modules/renderPages.js
@@ -0,0 +1,17 @@
+const login_form = () => {
+    return `
+        <section id="login">
+        <h1 class="exile-regular">battleship</h1>
+        <form action="#">
+          <input
+            type="text"
+            placeholder="Enter Captain's Name"
+            class="tagesschrift-regular"
+          /><br />
+          <button type="submit" class="tagesschrift-regular">
+            Comfirm Cap's Name
+          </button>
+        </form>
+      </section>
+    `
+}
