c6ba6402769bb8d0e529fae48c564b4de520c2f6
Johsam <johsamfcress@gmail.com>
2025-06-05 12:45:02 +0200

    added functions to handle placing of ships in respective game boards

--- src/modules/gameEngine.js
+++ src/modules/gameEngine.js
@@ -1,10 +1,19 @@
 import Player from "./player";
+import { ship_positions } from "./shipPlacement";
 
 const player1 = Player();
 const computer = Player();
 
-function setting_gameboard(player, index, length, is_vertical){
-    return player.player_board.place_ship({x: index % 10, y: 9 - Math.floor(index/10)}, length, is_vertical);
+function setting_gameboard(){
+    for (let i = 0; i < 5; i++) {
+        let {first_index, length, is_vertical} = ship_positions[i];
+        player1.player_board = player_gameboard(player1.player_board, first_index, length, is_vertical);
+    }
+    
+}
+
+function player_gameboard(player_board, index, length, is_vertical){
+    return player_board.place_ship({x: index % 10, y: 9 - Math.floor(index/10)}, length, is_vertical);
 }
 
 
