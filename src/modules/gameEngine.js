e068068c8317aae573910dc3e01d5c8a7d39a4d4
Johsam <johsamfcress@gmail.com>
2025-06-05 19:32:27 +0200

    removed useless code

--- src/modules/gameEngine.js
+++ src/modules/gameEngine.js
@@ -41,16 +41,7 @@ function align_ships(){
 
     const img_cells = document.querySelectorAll('.your-cell');
 
-    const ships = [
-        { name: 'carrier', length: 5 },
-        { name: 'battleship', length: 4 },
-        { name: 'cruiser', length: 3 },
-        { name: 'submarine', length: 3 },
-        { name: 'destroyer', length: 2 }
-      ];
-
     // Placement
-
     for (let i = 0; i < ship_positions.length; i++) {
         let {first_index, length, is_vertical} = ship_positions[i];
         img_cells.forEach( cell => {
@@ -59,48 +50,6 @@ function align_ships(){
             }
         });
     }
-    
-
-  grid.addEventListener('click', (e) => {
-    if (!selected_ship) return;
-    const index = parseInt(e.target.dataset.index);
-    const cells = _get_cells(index, selected_ship.length, is_vertical);
-    if (!_is_valid(cells)) return;
-
-    cells.forEach(i => {
-      const cell = document.querySelector(`[data-index="${i}"]`);
-      if (cell) {
-        cell.classList.add('occupied', selected_ship.name);
-        cell.dataset.ship = selected_ship.name;
-      }
-    });
-
-    // Add actual image into the first cell
-    const firstCell = document.querySelector(`[data-index="${cells[0]}"]`);
-    if (firstCell) {
-      const shipImg = document.createElement('img');
-      shipImg.src = shipImages[`${selected_ship.name}.png`];
-      shipImg.alt = selected_ship.name;
-      shipImg.classList.add('grid-ship', is_vertical);
-      shipImg.style.position = 'absolute';
-      shipImg.style.width = is_vertical === 'horizontal'
-        ? `${selected_ship.length * 100}%`
-        : '100%';
-    shipImg.style.height = is_vertical === 'vertical'
-        ? `${selected_ship.length * 100}%`
-        : '100%';
-      shipImg.style.objectFit = 'contain';
-      shipImg.style.top = '0';
-      shipImg.style.left = '0';
-      shipImg.style.zIndex = '5';
-
-      if (is_vertical === 'vertical') {
-        shipImg.style.transform = 'rotate(90deg)';
-      }
-
-      firstCell.appendChild(shipImg);
-    }
-});
 }
 
 
