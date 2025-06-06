fcb65453c5386194dafa64594447a034d8366cfd
Johsam <johsamfcress@gmail.com>
2025-06-06 17:08:59 +0200

    added logic for game play for both player and computer

--- src/modules/gameEngine.js
+++ src/modules/gameEngine.js
@@ -4,6 +4,7 @@ import { ship_positions, _add_img_to_cell, _get_cells } from "./shipPlacement";
 
 const player1 = Player();
 const computer = Player();
+let winner = '';
 
 const comp_ship_lengths = [5, 4, 3, 3, 2];
 
@@ -38,7 +39,6 @@ function _get_coord(index){
 }
 
 function align_ships(){
-    const img_cells = document.querySelectorAll('.your-cell');
     const ships = [
         { name: 'carrier'},
         { name: 'battleship'},
@@ -67,8 +67,87 @@ function align_ships(){
       }
     }
   }
+
+  function add_dot(cell){
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    cell.appendChild(dot);
+  }
+
+  function handle_computer_player(){
+    let attack_succeed = false;
+    do{
+        const index = Math.floor(Math.random() * 100);
+        const coord = _get_coord(index);
+        const player_cell = document.querySelector(`.your-cell[data-index="${index}"]`);
+
+        if(!player_cell.classList.contains("attacked")){
+            if(computer.attack(player1.player_board, coord)){
+                player_cell.classList.add("attack-succeed");
+            }else{
+                player_cell.classList.add("attack-fail");
+            }
+            player_cell.classList.add("attacked");
+
+            if(player_cell){
+                add_dot(player_cell);
+            }
+
+            if(player1.player_board.all_ships_sunk()){
+                winner = "computer";
+                document.dispatchEvent(
+                    new CustomEvent('gameEnded',{
+                        detail: { winner: "computer" } 
+                    })
+                );
+            }
+            attack_succeed = true;
+        }
+
+    }while(!attack_succeed);
+  }
+
+  function game_engine(){
+    setting_gameboard();
+    align_ships();
+
+    //game logic
+    document.getElementById("enemy-fleet").addEventListener('click', (e) => {
+        if(e.target.classList.contains("enemy-cell")){
+
+            if(e.target.classList.contains("attack-succeed") || e.target.classList.contains("attack-fail")) return;
+
+            const index = parseInt(e.target.dataset.index);
+            const coord = _get_coord(index);
+            const enemy_cell = document.querySelector(`.enemy-cell[data-index="${index}"]`);
+
+            if(player1.attack(computer.player_board, coord)){
+                e.target.classList.add("attack-succeed");
+            }else{
+                e.target.classList.add("attack-fail");
+            }
+
+            if(enemy_cell){
+                add_dot(enemy_cell);
+            }
+
+            if(computer.player_board.all_ships_sunk()){
+                winner = "player";
+                document.dispatchEvent(
+                    new CustomEvent('gameEnded',{
+                        detail: { winner: "player" } 
+                    })
+                );
+            }else{
+                setTimeout(() => {
+                    handle_computer_player();
+                }, 500);
+            }
+
+        }
+    });
+
+}
+
+  export {game_engine, winner};
   
-
-
-
-export { setting_gameboard, player1, computer, align_ships };
